b4e24a18b9d5deccfec3a08fe7a00374d36ee954
jwest <john.westbrook@rcsb.org>
2019-12-20 10:20:56 -0500

    V0.31 Adjust package definition

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ),
     entry_points={"console_scripts": ["exdb_exec_cli=rcsb.exdb.cli.ExDbExec:main"]},
     #
