8d130298ddedb85c78cab76041428d704ec1aa0e
jwest <john.westbrook@rcsb.org>
2021-05-26 20:17:59 -0400

    Feat: V0.74 Add modules GlycanProvider(), GlycanUtils() and associated tests

---
+++ rcsb/exdb/branch/GlycanUtils.py
@@ -0,0 +1,114 @@
+##
+#  File:           GlycanUtils.py
+#  Date:           24-May-2021 jdw
+#
+#  Updated:
+##
+"""
+Utilities for fetching and mapping glycan accessions.
+"""
+
+import logging
+import os.path
+
+from rcsb.exdb.branch.BranchedEntityExtractor import BranchedEntityExtractor
+from rcsb.utils.io.MarshalUtil import MarshalUtil
+from rcsb.utils.io.UrlRequestUtil import UrlRequestUtil
+
+logger = logging.getLogger(__name__)
+
+
+class GlycanUtils:
+    """Utilities for fetching and mapping glycan annotations."""
+
+    def __init__(self, cfgOb, dirPath, **kwargs):
+        self.__cfgOb = cfgOb
+        self.__dirPath = dirPath
+        self.__mU = MarshalUtil(workPath=self.__dirPath)
+        #
+
+    def __getRawGlycanDetailsPath(self):
+        return os.path.join(self.__dirPath, "pdb-raw-branched-entity-details.json")
+
+    def getBranchedEntityDetails(self):
+        """For branched entities, get BIRD mapping and WURCS details"""
+        ok = False
+        try:
+            bEx = BranchedEntityExtractor(self.__cfgOb)
+            branchedEntityD = bEx.getBranchedDetails()
+            logger.info("Branched entity descriptor details count %d", len(branchedEntityD))
+            detailsPath = self.__getRawGlycanDetailsPath()
+            ok = bEx.exportBranchedEntityDetails(detailsPath, fmt="json")
+            logger.info("Store raw branched entity data (%r) %s", ok, detailsPath)
+        except Exception as e:
+            logger.exception("Failing with %s", str(e))
+        #
+        return branchedEntityD
+
+    def __getGlycanAccessionMapPath(self):
+        return os.path.join(self.__dirPath, "accession-wurcs-mapping.json")
+
+    def fetchGlycanAccessionMap(self):
+        mapD = {}
+        accessionMapPath = self.__getGlycanAccessionMapPath()
+        if self.__mU.exists(accessionMapPath):
+            mapD = self.__mU.doImport(accessionMapPath, fmt="json")
+        return mapD
+
+    def storeGlycanAccessionMap(self, mapD):
+        accessionMapPath = self.__getGlycanAccessionMapPath()
+        ok = self.__mU.doExport(accessionMapPath, mapD, fmt="json", indent=3)
+        return ok
+
+    def updateEntityAccessionMap(self):
+        """Update entity to glycan accession mapping
+
+        Returns:
+            dict: {entityId: {'glyTouCanId':... , 'prdId': ..., }, ... }
+        """
+        entityAccessionMapD = {}
+        wurcsTupL = []
+        uniqueWurcsD = {}
+        accessionMapD = self.fetchGlycanAccessionMap()
+        branchedEntityD = self.getBranchedEntityDetails()
+        for entityId, iD in branchedEntityD.items():
+            if iD["wurcs"] and iD["wurcs"] not in accessionMapD and iD["wurcs"] not in uniqueWurcsD:
+                wurcsTupL.append((entityId, iD["wurcs"]))
+                uniqueWurcsD.setdefault(iD["wurcs"], []).append(entityId)
+        if wurcsTupL:
+            tMap = self.getAccessionMapping(wurcsTupL)
+            accessionMapD.update(tMap)
+            self.storeGlycanAccessionMap(accessionMapD)
+        #
+
+        for entityId, iD in branchedEntityD.items():
+            if iD["wurcs"] in accessionMapD:
+                prdId = iD["prdId"] if iD["wurcs"] else None
+                entityAccessionMapD[entityId] = {"glyTouCanId": accessionMapD[iD["wurcs"]][0], "prdId": prdId}
+        return entityAccessionMapD
+
+    def getAccessionMapping(self, wurcsTupL):
+        """Fetch GlyTouCan accessions for the input WURCS desriptor list"""
+        accessionMapD = {}
+        logger.info("Fetching (%d) WURCS descriptors", len(wurcsTupL))
+        baseUrl = "https://api.glycosmos.org"
+        endPoint = "glytoucan/sparql/wurcs2gtcids"
+        numDescriptors = len(wurcsTupL)
+        for ii, (entityId, wurcs) in enumerate(wurcsTupL, 1):
+            try:
+                pD = {}
+                pD["wurcs"] = wurcs
+                uR = UrlRequestUtil()
+                rDL, retCode = uR.post(baseUrl, endPoint, pD, returnContentType="JSON")
+                logger.debug(" %r wurcs fetch result (%r) %r", entityId, retCode, rDL)
+                if rDL:
+                    for rD in rDL:
+                        if "id" in rD:
+                            accessionMapD.setdefault(wurcs, []).append(rD["id"])
+                        else:
+                            logger.info("%r fetch fails (%r) (%r) %r", entityId, retCode, wurcs, rDL)
+                if ii % 5 == 0:
+                    logger.info("Fetched %d/%d", ii, numDescriptors)
+            except Exception as e:
+                logger.exception("Failing for (%r) wurcs (%r) with %s", entityId, wurcs, str(e))
+        return accessionMapD
---
+++ rcsb/exdb/tests/testGlycanUtils.py
@@ -0,0 +1,63 @@
+##
+# File:    GlycanUtilsTests.py
+# Author:  J. Westbrook
+# Date:    25-May-2021
+#
+# Update:
+##
+"""
+Tests for creating glycan accession mapping details.
+"""
+
+__docformat__ = "restructuredtext en"
+__author__ = "John Westbrook"
+__email__ = "jwest@rcsb.rutgers.edu"
+__license__ = "Apache 2.0"
+
+import logging
+import os
+import unittest
+
+from rcsb.exdb.branch.GlycanUtils import GlycanUtils
+from rcsb.utils.config.ConfigUtil import ConfigUtil
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+TOPDIR = os.path.dirname(os.path.dirname(HERE))
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s]-%(module)s.%(funcName)s: %(message)s")
+logger = logging.getLogger()
+
+
+class GlycanUtilsTests(unittest.TestCase):
+    def setUp(self):
+        self.__dirPath = os.path.join(HERE, "test-output", "CACHE", "glycan", "mapped_identifiers")
+        #
+        self.__mockTopPath = os.path.join(TOPDIR, "rcsb", "mock-data")
+        configPath = os.path.join(TOPDIR, "rcsb", "mock-data", "config", "dbload-setup-example.yml")
+        configName = "site_info_configuration"
+        self.__cfgOb = ConfigUtil(configPath=configPath, defaultSectionName=configName, mockTopPath=self.__mockTopPath)
+        #
+
+    def tearDown(self):
+        pass
+
+    def testGlycanMapping(self):
+        gU = GlycanUtils(self.__cfgOb, self.__dirPath)
+        beD = gU.getBranchedEntityDetails()
+        self.assertGreaterEqual(len(beD), 1)
+        logger.info("branched entity descriptor details (%d)", len(beD))
+        eaD = gU.updateEntityAccessionMap()
+        logger.info("updated entity accession map length (%d)", len(eaD))
+        self.assertGreaterEqual(len(eaD), 12)
+
+
+def glycanMappingSuite():
+    suiteSelect = unittest.TestSuite()
+    suiteSelect.addTest(GlycanUtilsTests("testGlycanMapping"))
+    return suiteSelect
+
+
+if __name__ == "__main__":
+
+    mySuite = glycanMappingSuite()
+    unittest.TextTestRunner(verbosity=2).run(mySuite)
